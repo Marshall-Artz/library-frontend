dd96f8841a30c00781c5e7a354ae9e00f7507f4c
Marshall-Artz <marshallarts12@gmail.com>
2022-07-11 00:55:06 -0500

    initial header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import { Box } from '@mui/system';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Box component="span" sx={{ p: 2, border: '1px solid white' }}>
+          <Button>Save</Button>
+        </Box>
         <p>testing</p>
-        <Box
-          sx={{
-            width: 300,
-            height: 300,
-            backgroundColor: 'primary.light'
-          }}
-        />
       </header>
     </div>
   );
