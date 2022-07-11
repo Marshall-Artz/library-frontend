7065a25d2ed189a47af1c4ac5904849a747d5ea6
Marshall-Artz <marshallarts12@gmail.com>
2022-07-11 01:41:14 -0500

    changed color styling of banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,11 +1,9 @@
+import './App.css';
 import { Box } from '@mui/system';
-import { Button } from '@mui/material';
 
 const Banner = () => {
   return (
-    <Box component="span" sx={{ p: 2, border: '1px solid white', marginX: 0}}>
-        <Button>Save</Button>
-    </Box>
+    <Box component="span" sx={{p: 2, border: '1px solid white', marginX: 0}}/>
   );
 }
 
