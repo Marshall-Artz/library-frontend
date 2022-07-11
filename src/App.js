b38d7f9b8228e8151196489f50174f6ad231cfad
Marshall-Artz <marshallarts12@gmail.com>
2022-07-11 00:51:39 -0500

    creating initial header box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Box } from '@mui/system';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>testing</p>
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: 'primary.light'
+          }}
+        />
       </header>
     </div>
   );
