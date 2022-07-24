95b5e41ac2bfe862b03bcd2c91b61c7f9ff96d72
Marshall-Artz <marshallarts12@gmail.com>
2022-07-24 15:16:03 -0500

    starting to initialize look

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Banner from './Banner';
+import { Box } from '@mui/system';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Banner/>
-        <p>testing</p>
+        <Box sx={{p: 2, border: '1px solid white', margin: 2}}>
+          
+        </Box>
       </header>
     </div>
   );
