5152208865fd326876b03749e1ee65d0d36bf663
Marshall-Artz <marshallarts12@gmail.com>
2022-08-03 20:45:18 -0500

    Updates

---
+++ precache-manifest.05a44f29d2c1c16c52334a79e99f39ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e108c87e2b883d3142f1",
+    "url": "Marshall-Artz.github.io/library-frontend,/static/css/main.8f038cf3.chunk.css"
+  },
+  {
+    "revision": "e108c87e2b883d3142f1",
+    "url": "Marshall-Artz.github.io/library-frontend,/static/js/main.e108c87e.chunk.js"
+  },
+  {
+    "revision": "90f743255ce3241dd1a5",
+    "url": "Marshall-Artz.github.io/library-frontend,/static/js/1.90f74325.chunk.js"
+  },
+  {
+    "revision": "83e161d563c46be6ca60",
+    "url": "Marshall-Artz.github.io/library-frontend,/static/js/2.83e161d5.chunk.js"
+  },
+  {
+    "revision": "18718ca6d24b2775e78a",
+    "url": "Marshall-Artz.github.io/library-frontend,/static/js/runtime~main.18718ca6.js"
+  },
+  {
+    "revision": "149f2830ea49f7624ff1e46f0a132c50",
+    "url": "Marshall-Artz.github.io/library-frontend,/static/media/unt_logo.149f2830.png"
+  },
+  {
+    "revision": "48a35e5040a62732ee966bd31e3ff2eb",
+    "url": "Marshall-Artz.github.io/library-frontend,/index.html"
+  }
+];
